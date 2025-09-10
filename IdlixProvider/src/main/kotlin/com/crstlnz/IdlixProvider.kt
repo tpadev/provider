8e8bd885cc028f7b3130b80391d23dea036ff396
tpadev <69703841+tpadev@users.noreply.github.com>
2025-09-10 19:00:00 +0700

    Update IdlixProvider.kt

--- IdlixProvider/src/main/kotlin/com/crstlnz/IdlixProvider.kt
+++ IdlixProvider/src/main/kotlin/com/crstlnz/IdlixProvider.kt
@@ -312,9 +312,9 @@ class IdlixProvider : MainAPI() {
         println(m3uLink)
         
         // Replace .txt at the end with .m3u8
-        if (m3uLink.endsWith(".txt")) {
-            m3uLink = m3uLink.substringBeforeLast(".") + ".m3u8"
-        }
+        //if (m3uLink.endsWith(".txt")) {
+        //    m3uLink = m3uLink.substringBeforeLast(".") + ".m3u8"
+        //}
 
 
         M3u8Helper.generateM3u8(
